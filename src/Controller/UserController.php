879e67e87ba1e09f648f4a54fad06f1edae06811
RKlott <proklott@gmail.com>
2026-02-26 16:13:34 +0100

    Mise en place du squelette de modification des utilisateurs

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController
 {
-    #[Route('/admin/user', name: 'app_user')]
+    #[Route('/admin/user', name: 'app_users')]
     public function index(UserRepository $repo): Response
     {
         $users = $repo->findAll();
@@ -19,5 +22,12 @@ final class UserController extends AbstractController
         ]);
     }
 
-    
+    #[Route('/admin/user/update/{id}', name: 'app_user_update')]
+    public function updateUser(User $user, EntityManagerInterface $entityManager, Request $request) : Response
+    {
+        //TODO: Essayer de faire la logique d'update d'utilisateur en récupérant bien les rôles
+    return $this->render('user/updateUser.html.twig', [
+
+    ]);
+    }
 }
