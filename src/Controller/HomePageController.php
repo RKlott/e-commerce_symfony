b6834b5648e6f48a85bd6432a51585faef74cc20
Thorsoll <william.cesto14@gmail.com>
2026-03-04 14:41:27 +0100

    commencement de laffichage sur la page d'accueil

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -23,10 +24,10 @@ final class HomePageController extends AbstractController
 // }
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(): Response
+    public function index(Product $product): Response
     {
         return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
+            'product' => $product,
         ]);
     }
 }
