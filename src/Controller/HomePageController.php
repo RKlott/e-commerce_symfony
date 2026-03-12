b9e9d8c1e0ef0014931a5e60a442469297daa861
RKlott <proklott@gmail.com>
2026-03-12 16:09:44 +0100

    ajout du système d'affichage des infos d'un produit depuis la page principale en cliquant sur le nom de la card

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -9,26 +9,23 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomePageController extends AbstractController
-// {
-//     #[Route('/', name: 'app_home_page')]
-//     public function index(): Response
-//     {
-//         $nomstudents=['Jérmie', 'Léa', 'Pierick', 'Davy'];
-//         $age =17;
-
-//         return $this->render('home_page/index.html.twig', [
-//             // 'controller_name' => 'HomePageController',
-//             'lesNoms' => $nomstudents,
-//             'age' => $age
-//         ]);
-//     }
-// }
 {
     #[Route('/', name: 'app_home_page', methods: ["GET"])]
     public function index(ProductRepository $productRepository): Response
     {
         return $this->render('home_page/index.html.twig', [
             'products' => $productRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/show/{id}', name: 'app_home_product_show', methods: ["GET"])]
+    public function show(Product $product, ProductRepository $productRepository) : Response
+    {
+        $latestProducts = $productRepository->findBy([], ['id' => 'DESC'], 5);
+
+        return $this->render('home_page/show.html.twig', [
+            'product' => $product,
+            'latestProducts' => $latestProducts
         ]);
     }
 }
